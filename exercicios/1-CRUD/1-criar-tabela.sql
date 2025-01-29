7cc641f2b8622f251d9d769bddb316cf7f21c3eb
wsousa53 <wesley.sousa00@yahoo.com>
2025-01-29 16:06:01 -0300

    Update 1-criar-tabela.sql EU SOU NOVO ALUNO

--- exercicios/1-CRUD/1-criar-tabela.sql
+++ exercicios/1-CRUD/1-criar-tabela.sql
@@ -1,1 +1,4 @@
--- Crie uma nova tabela no banco de dados
+CREATE TABLE salario_classe (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  nivel TEXT,
+  salario INTEGER)-- Crie uma nova tabela no banco de dados
